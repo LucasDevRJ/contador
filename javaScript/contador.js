6e5d28c2700dc9643714bf75a75176ec5c5fdba9
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-05-13 18:37:50 -0300

    Armazenando elementos em variáveis

---
+++ javaScript/contador.js
@@ -0,0 +1,5 @@
+let valor = document.getElementById("valor");
+let botaoIncremento = document.getElementById("botao-incremento");
+let botaoDecremento = document.getElementById("botao-decremento");
+
+console.log(botaoDecremento);
