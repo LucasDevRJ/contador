a9c930fc2abdfcb9c0d62af30618370c6b3b695c
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-05-13 18:46:13 -0300

    Valor incial do número

--- javaScript/contador.js
+++ javaScript/contador.js
@@ -1,5 +1,9 @@
-let valor = document.getElementById("valor");
+let campoValor = document.getElementById("valor");
 let botaoIncremento = document.getElementById("botao-incremento");
 let botaoDecremento = document.getElementById("botao-decremento");
 
-console.log(botaoDecremento);
+let valor = campoValor.value;
+valor = 0;
+
+console.log(valor);
+
